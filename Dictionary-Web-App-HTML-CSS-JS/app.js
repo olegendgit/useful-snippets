bba80375109f8cbf162007ffed2701165da1f46e
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-04-28 23:26:30 +0300

    Second-part-to-develope-App

---
+++ Dictionary-Web-App-HTML-CSS-JS/app.js
@@ -0,0 +1,4 @@
+const url = "https://dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-button");
