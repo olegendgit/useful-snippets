b79a4ca46a6edb549a69a07f08459683344f2bf2
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-05-26 22:37:48 +0300

    Make-functional-main-web-interface

--- Color-Maker-App/application.js
+++ Color-Maker-App/application.js
@@ -1,8 +1,8 @@
-const cols = document.querySelector('.col')
+const cols = document.querySelectorAll('.col')
 
 document.addEventListener('keydown', (event) => {
   event.preventDefault()
-  if (event.composed.toLowerCase() ==='space') {
+  if (event.code.toLowerCase() === 'space') {
     setRandomColors()
   }
 })
@@ -10,15 +10,16 @@ document.addEventListener('keydown', (event) => {
 document.addEventListener('click', (event) => {
   const type = event.target.dataset.type
 
-  if(type ==='lock') {
-    const node = event.target.tagName.toLowerCase() =='i'
-    ? event.target
-    :event.target.chidren[0]
+  if (type === 'lock') {
+    const node =
+      event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0]
 
     node.classList.toggle('fa-lock-open')
     node.classList.toggle('fa-lock')
   } else if (type === 'copy') {
-    copyToClickboard(evnt.target.textContent)
+    copyToClickboard(event.target.textContent)
   }
 })
 
@@ -36,11 +37,64 @@ function gerenerateRandomColor() {
   return '#' + color
 }
 
-
-function setToClickoard(text) {
-  cols.forEach((col) => {
-    col.style.background = gerenerateRandomColors()
-  })
+function copyToClickboard(text) {
+  return navigator.clipboard.writeText(text)
 }
 
-setRandomColors()
+function setRandomColors(isInitial) {
+  const colors = isInitial ? getColorsFromHash() : []
+
+  cols.forEach((col, index) => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock')
+    const text = col.querySelector('h2')
+    const button = col.querySelector('button')
+
+    if (isLocked) {
+      colors.push(text.textContent)
+      return
+    }
+
+    const color = isInitial
+      ? colors[index]
+        ? colors[index]
+        : chroma.random()
+      : chroma.random()
+
+    if (!isInitial) {
+      colors.push(color)
+    }
+
+    text.textContent = color
+    col.style.background = color
+
+    setTextColor(text, color)
+    setTextColor(button, color)
+  })
+
+  updateColorsHash(colors)
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance()
+  text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+function updateColorsHash(colors = []) {
+  document.location.hash = colors
+    .map((col) => {
+      return col.toString().substring(1)
+    })
+    .join('-')
+}
+
+function getColorsFromHash() {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1)
+      .split('-')
+      .map((color) => '#' + color)
+  }
+  return []
+}
+
+setRandomColors(true)
