424b944d07e503017866f448e879fb24b1d8eec6
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-05-31 23:48:39 +0300

    Delevelope-frontend-js-files

--- JavaScript-List-Item/server.js
+++ JavaScript-List-Item/server.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("frontend", index.html));
-})
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
-app.listen(process.env.PORT || 5060, () => console.log("Server running..."));
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
