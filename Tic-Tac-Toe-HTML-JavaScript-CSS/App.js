a533ecc3edd57881c2be9f96ab4c817e3a5f58ee
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-06-12 22:41:54 +0300

    Replace-basic-elements-to-JavaScript-file

---
+++ Tic-Tac-Toe-HTML-JavaScript-CSS/App.js
@@ -0,0 +1,27 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+
+const cellElements = document.querySelectorAll('[data-cell]') 
+let circleTurn
+
+cellElements.forEach(cell => {
+  cell.addEventListener('click', handleClick, { once: true})
+})
+
+function handleClick(e) {
+  const cell = e.target 
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  //Check For Win 
+  //Check For Draw
+  //Switch Turns
+  swapTurns()
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
