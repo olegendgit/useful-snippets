b43bfa80331198949bf4f33d3e2774287dc75683
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-06-14 12:25:05 +0300

    Made-an-alternation-of-the-main-elements-X-CIRCLE

--- Tic-Tac-Toe-HTML-JavaScript-CSS/App.js
+++ Tic-Tac-Toe-HTML-JavaScript-CSS/App.js
@@ -1,12 +1,18 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
-
-const cellElements = document.querySelectorAll('[data-cell]') 
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board') 
 let circleTurn
 
-cellElements.forEach(cell => {
-  cell.addEventListener('click', handleClick, { once: true})
-})
+startGame()
+
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true})
+  })
+  setBoardHoverClass()
+}
 
 function handleClick(e) {
   const cell = e.target 
@@ -16,6 +22,7 @@ function handleClick(e) {
   //Check For Draw
   //Switch Turns
   swapTurns()
+  setBoardHoverClass()
 }
 
 function placeMark(cell, currentClass) {
@@ -25,3 +32,18 @@ function placeMark(cell, currentClass) {
 function swapTurns() {
   circleTurn = !circleTurn
 }
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if(circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+
+
+
+
