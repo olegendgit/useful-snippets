68ebe62bd962175d8a7f2ad9a9b464224cd4bf15
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-06-15 22:32:33 +0300

    Make-the-message-for-person-who-win-in-the-game

--- Tic-Tac-Toe-HTML-JavaScript-CSS/App.js
+++ Tic-Tac-Toe-HTML-JavaScript-CSS/App.js
@@ -1,5 +1,15 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8], 
+  [0, 4, 8],
+  [2, 4, 6]
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board') 
 let circleTurn
@@ -18,6 +28,10 @@ function handleClick(e) {
   const cell = e.target 
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
   placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    console.log('winner')
+
+  }
   //Check For Win 
   //Check For Draw
   //Switch Turns
@@ -41,6 +55,14 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(X_CLASS)
   }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
 }
 
 
