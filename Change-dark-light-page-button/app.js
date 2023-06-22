7e4811059abe8ea7bd68e8fd9e37d0678087467d
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-06-23 00:11:22 +0300

    It-is-interesting-idea-for-future-projects

---
+++ Change-dark-light-page-button/app.js
@@ -0,0 +1,54 @@
+/**
+* Utility function to calculate the current theme setting.
+* Look for a local storage value.
+* Fall back to system setting.
+* Fall back to light mode.
+*/
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+        return localStorageTheme;
+    }
+
+    if (systemSettingDark.matches) {
+        return "dark";
+    }
+
+    return "light";
+}
+
+function updateButton({ buttonEl, isDark }) {
+    const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+    // use an aria-label if you are omitting text on the button
+    // and using a sun/moon icon, for example
+    buttonEl.setAttribute("aria-label", newCta);
+    buttonEl.innerText = newCta;
+}
+
+
+function updateThemeOnHtmlEl({ theme }) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+/**
+* 4. Add an event listener to toggle the theme
+*/
+button.addEventListener("click", (event) => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+    localStorage.setItem("theme", newTheme);
+    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+    updateThemeOnHtmlEl({ theme: newTheme });
+
+    currentThemeSetting = newTheme;
+}); 
