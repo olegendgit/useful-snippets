6490833b07edebb2b1acd21c22ec6afcea6490f9
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-07-02 20:28:40 +0300

    Change-style-css-and-add-elements-js

--- JavaScript-Quiz-App/app.js
+++ JavaScript-Quiz-App/app.js
@@ -32,3 +32,16 @@ const quizData = [
   correct: "b",
   },
   ];
+  const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById("submit");
+let currentQuiz = 0;
+let score = 0;
+const deselectAnswers = () => {
+answerElements.forEach((answer) => (answer.checked = false));
+};
