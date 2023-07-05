6f6853d19d4355352d9d36a85867c56f899ab73c
olegendgit <106254711+olegendgit@users.noreply.github.com>
2023-07-05 22:12:06 +0300

    complete-web-quiz-application

--- JavaScript-Quiz-App/app.js
+++ JavaScript-Quiz-App/app.js
@@ -33,15 +33,46 @@ const quizData = [
   },
   ];
   const quiz = document.getElementById("quiz");
-const answerElements = document.querySelectorAll(".answer");
-const questionElement = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitButton = document.getElementById("submit");
-let currentQuiz = 0;
-let score = 0;
-const deselectAnswers = () => {
-answerElements.forEach((answer) => (answer.checked = false));
-};
+  const answerElements = document.querySelectorAll(".answer");
+  const questionElement = document.getElementById("question");
+  const a_text = document.getElementById("a_text");
+  const b_text = document.getElementById("b_text");
+  const c_text = document.getElementById("c_text");
+  const d_text = document.getElementById("d_text");
+  const submitButton = document.getElementById("submit");
+  let currentQuiz = 0;
+  let score = 0;
+  const deselectAnswers = () => {
+  answerElements.forEach((answer) => (answer.checked = false));
+  };
+  const getSelected = () => {
+  let answer;
+  answerElements.forEach((answerElement) => {
+  if (answerElement.checked) answer = answerElement.id;
+  });
+  return answer;
+  };
+  const loadQuiz = () => {
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionElement.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+  };
+  loadQuiz();
+  submitButton.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+  if (answer === quizData[currentQuiz].correct) score++;
+  currentQuiz++;
+  if (currentQuiz < quizData.length) loadQuiz();
+  else {
+  quiz.innerHTML = `
+  <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+  <button onclick="history.go(0)">Play Again</button>
+  ` // location.reload() won't work in CodePen for security reasons;
+  }
+  }
+  });
